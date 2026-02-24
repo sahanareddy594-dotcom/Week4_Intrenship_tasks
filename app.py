091a58f8e638e9b655dd0d86e2e83284563d3b12
sahanareddy594-dotcom <sahanareddy594@gmail.com>
2026-02-24 18:49:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Welcome to Axilex Services Private ltd !"
+
+if __name__ == '__main__':
+    app.run(debug=True)
