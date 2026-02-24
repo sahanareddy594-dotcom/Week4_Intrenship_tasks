3e063cf7986c3f54fb02000046c34659f8e62675
sahanareddy594-dotcom <sahanareddy594@gmail.com>
2026-02-24 18:50:15 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Welcome to Axilex Services Private ltd !"
-
-if __name__ == '__main__':
-    app.run(debug=True)
